803dcafc64d3ee771f7da8d27c1f02cb71af3b8c
Nuruzzaman Khan Nabwab <mr.nuruzzamancse@gmail.com>
2017-08-26 21:36:46 +0600

    Method Over Loading

---
+++ Method_Overloading/src/Calculator.java
@@ -0,0 +1,21 @@
+
+public class Calculator {
+	
+	float performAddition(float a, float b){
+		
+		return a+b;
+	}
+	
+	int performAddition(int a , int b){
+		
+		return a+b;
+	}
+	
+	int performAddition(int a, int b, int c){
+		
+		return a+b+c;
+	}
+	
+
+
+}
