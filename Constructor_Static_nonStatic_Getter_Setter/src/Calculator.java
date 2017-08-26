2bf2f38b0a0a014c062b52bc1da8f4ff57d3cea3
Nuruzzaman Khan Nabwab <mr.nuruzzamancse@gmail.com>
2017-08-26 23:24:48 +0600

    N

---
+++ Constructor_Static_nonStatic_Getter_Setter/src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+
+	static int number = 6;
+	
+	char a = 'ক';
+	
+	public int  addition(int a, int b) {
+		return a+b;
+	}
+	 public static int square(int n) {
+		 
+		 
+		return n*n;
+	}
+}
