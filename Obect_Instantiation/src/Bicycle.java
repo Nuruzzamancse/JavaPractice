c88c68a30a7ffae8f5d5fc3b4cf7447eefc39388
Nuruzzaman Khan Nabwab <mr.nuruzzamancse@gmail.com>
2017-08-26 21:29:51 +0600

    Object Instantiation

---
+++ Obect_Instantiation/src/Bicycle.java
@@ -0,0 +1,25 @@
+public class Bicycle {
+	int cadence = 0;
+	int speed = 0;
+	int gear = 1;
+	
+	void changeCadence(int newValue){
+		cadence = newValue;
+	}
+	
+	void changeGear(int newValue){
+		gear = newValue;
+	}
+	
+	void speedUp(int increment){
+		speed = speed + increment;
+	}
+	
+	void applyBreaks(int decrement){
+		speed = speed - decrement;
+	}
+	
+	void printStates(){
+		System.out.println("Cadence: "+cadence + " speed: "+ speed + " gear: "+gear );
+	}
+}
